6ca438f2d20e40e143313943b95379c0a2032303
gustavo tessitore <gugatessi@gmail.com>
2018-05-08 11:46:34 -0300

    codigo comentado

--- projetao.py
+++ projetao.py
@@ -11,19 +11,25 @@ import pygame
 
 
 pygame.init()
+
+#Determina a altura e a largura da janela principal
 altura = 800
 largura = 600
 
+#Define o titulo da janela
 gameDisplay = pygame.display.set_mode((altura, largura))
 pygame.display.set_caption('Sword Quest')
 
-
+#Define cores RGB
 black = (0,0,0)
 white = (255,255,255)
 
 relogio = pygame.time.Clock()
+
+#Determina a condição do loop principal do jogo
 vivo = True
 
+#carrega as imagens do jogo
 boneco = pygame.image.load('boneco.png')
 boneco_mask = pygame.image.load('boneco_mask.png').convert_alpha()
 mapa = pygame.image.load('map.png')
@@ -42,6 +48,7 @@ saida_da_cidade = pygame.mask.from_surface(mapa_saidaC)
 
 bonecoM = pygame.mask.from_surface(boneco_mask)
 
+#Funções para colocar imagens na janela
 def posicao_boneco(x,y):
     gameDisplay.blit(boneco, (x,y))
     
@@ -59,52 +66,61 @@ def posicao_mapaS(x,y):
 
 def posicao_saida(x,y):
     gameDisplay.blit(mapa_saidaC, (x,y))
-    
-pygame.mixer.music.load('Overworld.ogg')
-pygame.mixer.music.play(-1)
-    
 
 
-
-
+#Posição inicial do boneco
 x_b =  (altura * 0.45)
 y_b = (largura * 0.45)
 
-x_m = -248
-y_m = -248
+#posição inicial do mapa
+x_m = -231
+y_m = -404
 
+#x_m_ é o eixo x do Mapa e x_m é o eixo x da mascara de colisão do mapa
 x_m_ = x_m
 y_m_ = y_m
 
 
+#velocidades(e = esquerda, d = direita c = cima, b = baixo)
 vxe = 0
 vyc = 0
 vxd = 0
 vyb = 0
 
+
+#inicia o loop da musica 
+pygame.mixer.music.load('Overworld.ogg')
+pygame.mixer.music.play(-1)
+
+#Loop principal do jogo
 while vivo:
     
+    #resultado é a variavel que define se existe colisão entre o boneco e o mapa
     offset = (int(x_b - x_m), int(y_b - y_m))
     resultado = mascara_mapa.overlap(bonecoM, offset)
     
+    #define se existe contado entre a mascara 'sair da cidade' e o boneco
     sair_da_cidade = saida_da_cidade.overlap(bonecoM, offset)
     
+    
+    #Atualiza a posição da mascara do mapa
+    if x_m != x_m_:
+        x_m = x_m_
+
+    if y_m != y_m_:
+        y_m = y_m_
+        
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             vivo = False
             
-        if x_m != x_m_:
-            x_m = x_m_
-
-        if y_m != y_m_:
-            y_m = y_m_ 
-
+            
+        #Adiciona velocidade ao mapa
         if event.type == pygame.KEYDOWN:
                                        
             if event.key == pygame.K_a:                    
                 vxe = 1
-               
-                                    
+                                                   
             if event.key == pygame.K_d: 
                 vxd = 1
                                    
@@ -131,7 +147,7 @@ while vivo:
                 
             
             
-                  
+    #move a mascara do mapa e verifica se existe colisão, para depois mover o mapa           
     x_m -= vxd
 
     if not resultado:
@@ -154,11 +170,11 @@ while vivo:
    
     #print(resultado)     
     #print(x_b, y_b)
-    #print(x_m, y_m, 'm')
+    #print(x_m_, y_m_, 'mapa')
     
 
-
-    gameDisplay.fill(white)
+    #Coloca as imagens na janela
+    gameDisplay.fill(black)
     
     posicao_mapa_mask(x_m, y_m)
     posicao_saida(x_m, y_m)
@@ -172,6 +188,11 @@ while vivo:
         pygame.mixer.music.load('boraBatalha.ogg')
         pygame.mixer.music.play(-1)
         
+        x_m = 1000
+        y_m = 1000
+        x_m_ = x_m
+        y_m_ = y_m
+        
         
         green = (0, 153, 76)
         
@@ -179,7 +200,7 @@ while vivo:
         posicao_boneco_mask(x_b, y_b)
         posicao_boneco(x_b, y_b)
         
-    sair_da_cidade = False       
+       
         
     pygame.display.update()
     relogio.tick(1000)
